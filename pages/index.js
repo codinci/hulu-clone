224f7d9698a2c41b9443ece1b42e77f4f1b86722
codinci <dkibau@gmail.com>
2022-05-19 22:45:42 +0300

    Loading images of movies

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Body from '../components/body'
 import Header from '../components/header'
 import Navbar from '../components/navbar'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import requests from '../utils/requests'
+
+export default function Home({ results }) {
+
   return (
     <div>
       <Head>
@@ -14,6 +17,23 @@ export default function Home() {
       </Head>
         <Header />
         <Navbar />
+        <Body results = {results} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
